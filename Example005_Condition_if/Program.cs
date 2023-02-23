d8dbf96a91873fe42c409bc1a01e848bb2328c72
Alexey <asaleseksesey12345@gmail.com>
2023-02-23 14:23:52 +0300

    add Examples005 Condition if

---
+++ Example005_Condition_if/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "поля"){
+    Console.WriteLine("Толстенькая дрочила Привет!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
