4d83965fefee5af636d56e97a43e0c546740c7c5
Alexey <asaleseksesey12345@gmail.com>
2023-02-27 15:53:36 +0300

    maximum of 9 numbers

---
+++ Lection2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Поиск максимума из 9 чисел
+/*
+int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 3;
+int b1 = 6;
+int c1 = 9;
+int a2 = 15;
+int b2 = -6;
+int c2 = 19;
+int a3 = 26;
+int b3 = 7;
+int c3 = -2;
+
+// int max1 = max(a1, a2, a3);
+// int max2 = max(b1, b2, b3);
+// int max3 = max(c1, c2, c3);
+int maximum = max(max(a1, a2, a3), max(b1, b2, b3), max(c1, c2, c3));
+Console.WriteLine(maximum);
+*/
+
