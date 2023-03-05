f7bd5de55a82688c40f76e7397d39f32e0b0bdd3
Alexey <asaleseksesey12345@gmail.com>
2023-03-05 20:19:21 +0300

    Lection3

---
+++ Lection3/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+for (int i = 2; i<=10; i++)
+{
+    for (int j = 2; j<=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+
+int[] arr = { 1, 5, 4, 3, 1, 5, 6, 7, 9, 1, 5 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortMinToMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+void SortMaxToMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+}
+PrintArray(arr);
+SortMinToMax(arr);
+PrintArray(arr);
+SortMaxToMin(arr);
+PrintArray(arr);
